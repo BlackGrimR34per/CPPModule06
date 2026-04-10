0e3c3127cc20603e8639260a2fbe9aecaa0a27b4
Yohen Sheraun <black@Yohens-MacBook-Air.local>
2026-04-10 14:40:55 +0800

    Completed ex00, no MAKEFILE

---
+++ ex00/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/04 13:43:21 by yosherau          #+#    #+#             */
+/*   Updated: 2026/04/10 14:35:46 by yosherau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./ScalarConverter.hpp"
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 2)
+		return (EXIT_FAILURE);
+	std::string input = argv[1];
+	ScalarConverter::convert(input);
+}
